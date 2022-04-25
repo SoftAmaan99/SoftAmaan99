a5a4478823849ba5b404ad168e78a55bc636bdeb
Mohammed <amaan894939@gmail.com>
2022-04-25 23:06:44 +0530

    commit for pattern

---
+++ Pattern2.java
@@ -0,0 +1,26 @@
+// Hollow Rectangle :-
+/*
+        *****
+        *   *
+        *   *
+        *****
+*/
+public class Pattern2 {
+    public static void main(String[] args) {
+
+        int n = 4, m = 5;
+
+        // Outer loop
+        for (int i = 1; i <= n; i++) {
+            // Inner loop
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
